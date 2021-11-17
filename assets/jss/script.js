c8b82c30bd36ed32d56c3911a2550a2216036f44
sirteddanson <knowledgeMED@gmail.com>
2021-11-17 15:32:59 -0500

    built character array logic

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
 
+var useLowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var useUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var useNumbers = ['0','1','2','3','4','5','6','7','8','9'];
+var useSpecialChar = ['!','@','#','$','%','&','*','(',')','_','+','-','=','[',']','|',',','.','/','?','>','<'];
+
+
+
+var parametersFunction = function (){
+  var parameterArray = [];
+  var passwordParameters = parameterArray.concat(useUpperCase,useLowerCase, useNumbers, useSpecialChar);
+  console.log(passwordParameters);
+  debugger;
+}
+ //---------------------------------------------------------------------------------
 // 1st window LENGTH
   var generatePassword = function () {
     
@@ -14,17 +28,19 @@
       return generatePassword();
     }
 };
+ //---------------------------------------------------------------------------------
 // 2nd window LOWERCASE
 var passwordLowerCasePrompt = function() {
   var promptLowerCase = window.prompt("Would you like to include LOWERCASE letters? YES or NO");
-      promptLowerCase = promptLowerCase.toLowerCase();
-
-    if (promptLowerCase == "no" || promptLowerCase == null ) {
-      useLowerCase = false;
+      
+    if (promptLowerCase == "no" || promptLowerCase == "NO" || promptLowerCase == null ) {
+      useLowerCase = "";
       passwordUpperCasePrompt ();
     }
+    promptLowerCase = promptLowerCase.toLowerCase();
+
     if (promptLowerCase == "yes") {
-      useLowerCase = true;
+      useLowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
       passwordUpperCasePrompt ();
     }
     else
@@ -33,18 +49,22 @@ var passwordLowerCasePrompt = function() {
       return passwordLowerCasePrompt();
     }
 };
+ //---------------------------------------------------------------------------------
 // 3rd window UPPERCASE
 var passwordUpperCasePrompt = function() {
   var promptUpperCase = window.prompt("Would you like to include UPPERCASE letters? YES or NO");
-      promptUpperCase = promptUpperCase.toLowerCase();
+      
  
-      if (promptUpperCase == "no" || promptUpperCase == null ) {
-        useUpperCase = false;
-        passwordNumberPrompt ();
+      if (promptUpperCase == "no" || promptUpperCase == "NO" || promptUpperCase == null ) {
+        useUpperCase = "";
+        passwordNumbersPrompt();
       }
-      if (promptUpperCase == "yes") {
-      useUpperCase = true;
-       passwordNumberPrompt ();
+      
+      promptUpperCase = promptUpperCase.toLowerCase();
+
+      if (promptUpperCase === "yes") {
+       useUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+       passwordNumbersPrompt();
       }
       else
       {
@@ -52,44 +72,52 @@ var passwordUpperCasePrompt = function() {
       return passwordUpperCasePrompt();
       }
   };
+ //---------------------------------------------------------------------------------
 // 4th window NUMBERS
-var passwordNumberPrompt = function() {
-  var promptNumbers= window.prompt("Would you like to include NUMBERS? YES or NO");
+var passwordNumbersPrompt = function() {
+  var promptNumbers = window.prompt("Would you like to include Numbers letters? YES or NO");
+      
+ 
+      if (promptNumbers == "no" || promptNumbers == "NO" || promptNumbers == null ) {
+        useNumbers = "";
+        passwordSpecialCharPrompt();
+      }
+      
       promptNumbers = promptNumbers.toLowerCase();
 
-      if (promptNumbers == "no" || promptNumbers == null ) {
-        useNumbers = false;
-        passwordSpecialPrompt ();
-      }
-      if (promptNumbers == "yes") {
-      useNumbers = true;
-       passwordSpecialPrompt ();
+      if (promptNumbers === "yes") {
+       useNumbers = ['0','1','2','3','4','5','6','7','8','9'];
+       passwordSpecialCharPrompt();
       }
       else
       {
       window.alert("Please enter an appropriate value.");
-      return passwordNumberPrompt();
+      return passwordNumbersPrompt();
       }
-};
+  };
+ //---------------------------------------------------------------------------------
 // 5th window SPECIAL CHARACTERS
-var passwordSpecialPrompt = function() {
-  var promptSpecialChar= window.prompt("Would you like to include SPECIAL characters? YES or N0");
-    promptSpecialChar = promptSpecialChar.toLowerCase();
+var passwordSpecialCharPrompt = function() {
+  var promptSpecialChar = window.prompt("Would you like to include SpecialChar letters? YES or NO");
+      
+ 
+      if (promptSpecialChar == "no" || promptSpecialChar == "NO" || promptSpecialChar == null ) {
+        useSpecialChar = "";
+        parametersFunction();
+      }
+      
+      promptSpecialChar = promptSpecialChar.toLowerCase();
 
-    if (promptSpecialChar == "no" || promptSpecialChar == null ) {
-      useSpecialChar = false;
-      // Next Function
-    }
-    if (promptSpecialChar == "yes") {
-      useSpecialChar = true;
-      // Next Function
-    }
-    else
-    {
-    window.alert("Please enter an appropriate value.");
-    return passwordSpecialPrompt();
-    }
-};
+      if (promptSpecialChar === "yes") {
+       useSpecialChar = ['!','@','#','$','%','&','*','(',')','_','+','-','=','[',']','|',',','.','/','?','>','<'];
+       parametersFunction();
+      }
+      else
+      {
+      window.alert("Please enter an appropriate value.");
+      return passwordSpecialCharPrompt();
+      }
+  };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
